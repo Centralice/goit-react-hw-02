3b8d4aaf08035a7cddb41bc8e2795336bdc9bb14
Centralice <108938330+Centralice@users.noreply.github.com>
2024-07-28 12:33:01 +0200

    Крок 6 in progress

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,12 @@
 import s from "./Feedback.module.css";
 
-const Feedback = ({ feedback, totalFeedback }) => {
+const Feedback = ({ feedback, positiveFeedback }) => {
   return (
     <div>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>{positiveFeedback}% of our customers are perfectly happy!</p>
     </div>
   );
 };
